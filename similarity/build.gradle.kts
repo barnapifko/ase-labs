39d4018dd592bfc437765797f0cfd80c689b0cc8
barnapifko <pifko.barna@gmail.com>
2024-09-18 15:42:21 +0200

    Labor haladas

--- similarity/build.gradle.kts
+++ similarity/build.gradle.kts
@@ -1,11 +1,5 @@
 plugins {
-    java
-    application
-    jacoco
-}
-
-java.toolchain {
-    languageVersion.set(JavaLanguageVersion.of(21))
+    id("hu.bme.mit.ase.shingler.gradle.application")
 }
 
 repositories {
@@ -18,6 +12,9 @@ val log4jVersion = "2.23.1"
 val junitVersion = "5.10.0"
 
 dependencies {
+    implementation(project(":lib"))
+    implementation(project(":logic"))
+
     implementation("org.slf4j:slf4j-api:$slf4jVersion")
     implementation("info.picocli:picocli:$picoCliVersion")
 
@@ -26,24 +23,4 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
 
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
-}
-
-application {
-    mainClass = "hu.bme.mit.ase.shingler.similarity.SimilarityApp"
-}
-
-tasks.distZip {
-    enabled = false
-}
-
-tasks {
-    test {
-        useJUnitPlatform()
-        testLogging.showStandardStreams = true
-        finalizedBy(jacocoTestReport)
-    }
-
-    jacocoTestReport {
-        inputs.files(test.get().outputs)
-    }
 }
